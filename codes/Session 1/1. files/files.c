eff645b30589494c9b93dd7daf843c6f2119bfd1
yvbal <70612985+Yves242@users.noreply.github.com>
2024-09-17 13:08:50 +0800

    before new commit

--- codes/Session 1/1. files/files.c
+++ codes/Session 1/1. files/files.c
@@ -110,22 +110,8 @@ void removeFile() {
 }
 
 
-#include <dirent.h> 
 
-int main(void) {
-  DIR *d;
-  struct dirent *dir;
-  d = opendir(".");
-  if (d) {
-    while ((dir = readdir(d)) != NULL) {
-      printf("%s\n", dir->d_name);
-    }
-    closedir(d);
-  }
-  return(0);
-}
-
-void xmain() {
+void main() {
 
     // variable declarations
     char choice[10];
